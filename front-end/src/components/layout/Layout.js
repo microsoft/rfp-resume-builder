eaf4d5647bf3abe7d870e79d0ef7f06fd8d280c6
Paul Maneesilasan <paulmanee@microsoft.com>
2024-08-29 08:32:31 -0400

    fix page layout issue with set screen height

--- front-end/src/components/layout/Layout.js
+++ front-end/src/components/layout/Layout.js
@@ -3,7 +3,7 @@ import Sidebar from './Sidebar';
 
 const Layout = ({ children, pages, activePage, setActivePage }) => {
   return (
-    <div className="flex h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white">
+    <div className="flex min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white">
       <Sidebar pages={pages} activePage={activePage} setActivePage={setActivePage} />
       <div className="flex-1 p-10">
         {children}
